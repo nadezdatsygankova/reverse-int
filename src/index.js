c34f1173baa0cdd5fcbecbe05cef4644e4cbff96
Nadezda <nadia_pritchina@gmail.com>
2021-03-15 20:20:19 -0400

    fix: done solition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let string = n.toString();
+    let letters = string.split('');
+    let rever = letters.reverse();
+    let newstring = rever.join('');
+
+
+    return (
+        parseFloat(newstring))
+
 }
